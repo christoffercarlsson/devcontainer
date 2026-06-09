ef276f9a7a3c4d399cabfc734b60b13bbbecd2a0
Christoffer Carlsson <cc@christoffercarlsson.com>
2026-06-09 22:01:10 +0200

    Clean up base image environment

--- src/base/scripts/local.bash
+++ src/base/scripts/local.bash
@@ -1,3 +1,10 @@
+export BAT_THEME="base16"
+export EDITOR="nvim"
+export VISUAL="nvim"
+export TERM="xterm-256color"
+export LANG="en_US.UTF-8"
+export LC_ALL="$LANG"
+
 if [ -n "$SSH_TTY" ] && [ "$SHLVL" -eq 1 ]; then
     cd /workspace && t
 fi
--- src/base/scripts/local.fish
+++ src/base/scripts/local.fish
@@ -1,3 +1,10 @@
+set -gx BAT_THEME base16
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+set -gx TERM xterm-256color
+set -gx LANG en_US.UTF-8
+set -gx LC_ALL $LANG
+
 if set -q SSH_TTY; and test "$SHLVL" -eq 1
     cd /workspace; and t
 end
